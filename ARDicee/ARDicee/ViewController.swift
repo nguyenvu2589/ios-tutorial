dac78c738360cd7fd938d434d0732202f93a5319
Vu Nguyen <nguyenvu2589@gmail.com>
2020-07-11 23:01:33 -0600

    dicee-ar

---
+++ ARDicee/ARDicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARDicee
+//
+//  Created by Vu Nguyen on 7/10/20.
+//  Copyright © 2020 Vu Nguyen. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
